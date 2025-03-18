8ae83a824f6fee6053e8074f6127f5c4752f3d7d
ST10249469 <ST10249469@vcconnect.edu.za>
2025-03-18 14:40:02 +0200

    Second Commit

--- SubTestThree/Program.cs
+++ SubTestThree/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Mr World!");
+            Console.WriteLine("Hello, Mr and Mrs World!");
         }
     }
 }
