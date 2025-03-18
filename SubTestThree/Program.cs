8e434eb7920f7fc8936b7ab38c01db272a1438a0
ST10249469 <ST10249469@vcconnect.edu.za>
2025-03-18 14:55:17 +0200

    Final Commit

--- SubTestThree/Program.cs
+++ SubTestThree/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Mr and Mrs World!");
+            Console.WriteLine("Hello, Mr Muhammed Hoosen!");
         }
     }
 }
